43f57fe3991360750cb009f68183a7c7cc133274
shubham088 <shubhamsharma0296@gmail.com>
2019-05-05 14:41:54 +0530

    Add files via upload

---
+++ FaceDetector.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+while(True):
+# Capture frame-by-frame
+    ret,frame = cap.read()
+
+# Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#faces coordinates
+    faces = face_cascade.detectMultiScale(gray, 1.05, 5)
+    print 'faces:{0}'.format(faces)
+    #drawing rectangle around face
+    for x,y,w,h in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if len(eyes) == 0:
+            print('ALERT ALERT ALERT ALERT ALERT ALERT')
+        print 'eyes : {0}'.format(eyes)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+# Display the resulting frame
+    cv2.imshow('Face Detector',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
